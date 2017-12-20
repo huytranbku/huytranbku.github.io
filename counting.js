eb32599edfe29efb77cb2262b53b43d6b74a566d
huytranbku <huy.tran@knorex.com>
2017-12-20 23:04:39 +0700

    add counting script

---
+++ counting.js
@@ -0,0 +1,30 @@
+Date.prototype.yyyymmdd = function() {
+  var mm = this.getMonth() + 1; // getMonth() is zero-based
+  var dd = this.getDate();
+
+  return [this.getFullYear(),
+          (mm>9 ? '' : '0') + mm,
+          (dd>9 ? '' : '0') + dd
+         ].join('');
+};
+document.addEventListener("DOMContentLoaded", function(event) {
+  var config = {
+    apiKey: "AIzaSyBOKZlstMKI1-i50rtxlQJmx87WEMBMWZ4",
+    authDomain: "myblog-f89d9.firebaseapp.com",
+    databaseURL: "https://myblog-f89d9.firebaseio.com",
+    projectId: "myblog-f89d9",
+    storageBucket: "myblog-f89d9.appspot.com",
+    messagingSenderId: "584819075341"
+  };
+  firebase.initializeApp(config);
+
+  var d = new Date();
+  var temp = d.yyyymmdd() + "_" + d.toTimeString().substring(0,8) + "_" + d.getTime();
+  var n = temp.replace(/[^a-z0-9\_]/gi,'');
+  var source = window.location.hostname;
+  var hostname = source.replace(/[^a-z0-9]/gi,'');
+
+  firebase.database().ref('counting/' + hostname + "/" + n  ).set({
+    time: d.toLocaleString()
+  });
+});
