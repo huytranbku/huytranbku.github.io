d73adf426cb592ee6d63cf7e9b40c3c4938504d8
huytran <tranthehuy.2nd@gmail.com>
2020-08-09 19:36:17 +0700

    update homepage

---
+++ build/precache-manifest.68d25222aecdf5ae59437591dd79f9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354e549bfa74437eb44a7edf631365c9",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "55fc363667fc0fc1d997",
+    "url": "/dist/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "3fa15140e5b001dbf7bc",
+    "url": "/dist/static/js/2.77c0ba04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dist/static/js/2.77c0ba04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fc363667fc0fc1d997",
+    "url": "/dist/static/js/main.19dfb6f3.chunk.js"
+  },
+  {
+    "revision": "19341d0275df722e2106",
+    "url": "/dist/static/js/runtime-main.f0ac9ea4.js"
+  }
+]);
